56487b838f065dc6b4bd6bd34376718febea86a3
Long Nguyen <longnewint@gmail.com>
2024-12-14 23:00:51 -0500

    Changed RestForm to RestParam and BeanParam

--- src/main/java/newint/northwind/api/CustomerApi.java
+++ src/main/java/newint/northwind/api/CustomerApi.java
@@ -2,11 +2,11 @@ package newint.northwind.api;
 
 import java.util.List;
 
-import org.jboss.resteasy.reactive.RestForm;
-
 import jakarta.inject.Inject;
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -34,11 +34,10 @@ public class CustomerApi {
 
   @POST
   @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public String post(@RestForm String id, @RestForm String company, @RestForm String name,
-  @RestForm String title, @RestForm String address, @RestForm String city, @RestForm String region,
-  @RestForm String postal_code, @RestForm String country, @RestForm String phone, @RestForm String fax) {
-    Customer c = new Customer(id, company, name, title, address, city, region, postal_code,
-      country, phone, fax);
+  public String post(@BeanParam CustomerFormParam param) {
+    Customer c = new Customer(param.id, param.company, param.name, param.title, param.address,
+    param.city, param.region, param.postal_code, param.country, param.phone, param.fax);
+
     repo.add(c);
 
     return "ok";
@@ -50,5 +49,19 @@ public class CustomerApi {
     repo.remove(id);
 
     return "ok";
+  }
+
+  public static class CustomerFormParam {
+    @FormParam("id") String id;
+    @FormParam("company") String company;
+    @FormParam("name") String name;
+    @FormParam("title") String title;
+    @FormParam("address") String address;
+    @FormParam("city") String city;
+    @FormParam("region") String region;
+    @FormParam("postal_code") String postal_code;
+    @FormParam("country") String country;
+    @FormParam("phone") String phone;
+    @FormParam("fax") String fax;
   }
 }
